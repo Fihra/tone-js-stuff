500ad259f6486c328af23aa4668aa7140ad6714d
Fihra <fabrofabian@gmail.com>
2020-09-03 16:46:40 -0700

    added envelope ADSR

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import * as Tone from 'tone';
 import { Piano, MidiNumbers, KeyboardShortcuts } from 'react-piano';
 import 'react-piano/dist/styles.css';
@@ -38,37 +38,56 @@ const keyboardShortcuts = KeyboardShortcuts.create({
     lastNote: lastNote,
     keyboardConfig: KeyboardShortcuts.HOME_ROW,
 });
-const volumeManager = {
-    vol: new Tone.Volume({
-        volume: 10
+
+const PanningManager = {
+    panner: new Tone.Panner({
+        pan: 1
     })
-};
+}
+
+let testPanner = new Tone.Panner(-1);
 
 const Keyboard = () => {
 
     const [osc, setOsc] = useState(SynthManager);
-    // const [volume, setVolume] = useState(volumeManager);
     const [volume, setVolume] = useState(10);
+    const [panLevel, setPanLevel] = useState(PanningManager);
+
+    const [attackLevel, setAttackLevel] = useState(0);
+    const [decayLevel, setDecayLevel] = useState(0);
+    const [sustainLevel, setSustainLevel] = useState(0);
+    const [releaseLevel, setReleaseLevel] = useState(0);
 
     useEffect(() => {
-        // mainVolume = new Tone.Volume({
-        //     volume: volume
-        // });
+
     }, [])
 
     const playNote = (note) => {
         const timeNow = Tone.now();
         let savedNote = Tone.Frequency(note, "midi").toNote();
-        // osc.main.chain(volumeManager.vol)
         osc.main.volume.value = volume;
+        
+        // osc.main.chain(testPanner);
+
+        osc.main.set({
+            envelope: {
+                attack: attackLevel,
+                decay: decayLevel,
+                sustain: sustainLevel,
+                release: releaseLevel
+            }
+        })
+
         osc.main.triggerAttack(savedNote, timeNow).toDestination();
+
     }
 
     const stopNote = (note) => {
         const timeNow = Tone.now();
         let savedNote = Tone.Frequency(note, "midi").toNote();
-        // osc.main.chain(volumeManager.vol)
         osc.main.volume.value = volume;
+
+        // osc.main.disconnect(testPanner);
         osc.main.triggerRelease(savedNote, timeNow + 0.1).toDestination();
     }
 
@@ -86,12 +105,44 @@ const Keyboard = () => {
 
     const handleVolume = (event) => {
         event.persist();
-        // let newVolume = new Tone.Volume({
-        //     volume: event.target.value
-        // })
-        // setVolume({vol: newVolume})
         setVolume(event.target.value);
     }
+    
+    const handlePan = (event) => {
+        // setPanLevel({...panLevel.panner, pan: event.target.value});
+        event.persist();
+        let result = new Tone.Panner({
+            pan: event.target.value
+        })
+        
+        setPanLevel({panner: result});
+    }   
+    // console.log(panLevel.panner.pan.value)
+
+    const handleAttack = (e) => {
+        setAttackLevel(e.target.value);
+    }
+
+    const handleDecay = (e) => {
+        setDecayLevel(e.target.value);
+    }
+
+    const handleSustain = (e) => {
+        setSustainLevel(e.target.value);
+    }
+
+    const handleRelease = (e) => {
+        setReleaseLevel(e.target.value);
+    }
+
+    // const canvasRef = useRef(null);
+    // const canvasObj = canvasRef.current;
+    // const ctx = canvasObj.getContext('2d');
+
+    // const drawGraph = () => {
+    //     ctx.fillStyle = 'blue';
+    //     ctx.save();
+    // }
 
     return(
         <div>
@@ -107,11 +158,29 @@ const Keyboard = () => {
                 {SynthChoice()}
             </select>
             <div className="controls-container">
-            {/* <label>Volume: {volume.vol._unmutedVolume}</label> */}
-            <label>Volume: {volume}</label>
-
-            {/* <input type='range' min='-60' max='100' value={volume.vol._unmutedVolume} onChange={(e) => handleVolume(e)}></input> */}
+            <label>Volume: {volume} db</label>
             <input type='range' min='-60' max='100' value={volume} onChange={(e) => handleVolume(e)}></input>
+            <label>Pan: {panLevel.panner.pan.value} </label>
+            <input type ='range' min='-1' max='1' value={panLevel.panner.pan.value} step='0.1' onChange={(e) => handlePan(e)}></input>
+
+            {/* <label>Panning: {} </label>
+            <input type ='range' min='-1' max='1' value={0.5} step='0.1' onChange={(e) => handlePan(e)}></input> */}
+            <div>
+                <label>Envelope</label>
+                <div>
+                    <label>Attack: {attackLevel}</label>
+                    <input type='range' min='0' max='1' value={attackLevel} step='0.01' onChange={(e) => handleAttack(e)}></input>
+
+                    <label>Decay: {decayLevel}</label>
+                    <input type='range' min='0' max='1' value={decayLevel} step='0.01' onChange={(e) => handleDecay(e)}></input>
+
+                    <label>Sustain: {sustainLevel}</label>
+                    <input type='range' min='0' max='1' value={sustainLevel} step='0.01' onChange={(e) => handleSustain(e)}></input>
+
+                    <label>Release: {releaseLevel}</label>
+                    <input type='range' min='0' max='1' value={releaseLevel} step='0.01' onChange={(e) => handleRelease(e)}></input>
+                </div>
+            </div>
 
             </div>
             
