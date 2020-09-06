3c83c26bbbfb95a949cae6da0ac86b09380b9e77
Fihra <fabrofabian@gmail.com>
2020-09-05 20:20:57 -0700

    still fixing up accidental feature

--- src/components/Composition.js
+++ src/components/Composition.js
@@ -16,7 +16,7 @@ const Composition = () => {
     const [musicNotes, setMusicNotes] = useState(notes);
 
     const synthPart = new Tone.Sequence( (time, note) => {
-        console.log(time);
+        // console.log(time);
         plucker.triggerAttackRelease(note, "10hz", time + 0.1);
     }, musicNotes, "16n");
 
@@ -28,7 +28,7 @@ const Composition = () => {
             Tone.Transport.start();
             // loopBeat.start(0);
         } else {
-            console.log("stopppppp");
+            // console.log("stopppppp");
             synthPart.stop();
             
             Tone.Transport.cancel(0);
@@ -36,27 +36,6 @@ const Composition = () => {
 
         }
     }, [isPlaying])
-
-    const song = (time) => { 
-        // if(counter%4 === 0){
-        //     plucker.triggerAttackRelease("C2", "8n", time);  
-        //     const seq = new Tone.Sequence((time, note) => {
-        //         console.log(time);
-        //         plucker.triggerAttackRelease(note, '4n'); 
-        //     }, notes, '8n');
-        //     seq.start(0);
-        // }
-        
-        // if(counter%2 === 0) {
-        //     duo.triggerAttackRelease("G4", '16n', time, 0.3);
-        // }
-
-        // plucker.triggerAttackRelease("Eb2", "8n", time);
-       
-        // console.log(Tone.Transport.position);
-
-        counter = (counter + 1) % 16
-    }
 
     const showAvailableNotes = (note) => {
         return noteChoices.map(nc => {
@@ -68,31 +47,83 @@ const Composition = () => {
         })
     }
 
+    const joinNotes = (givenNotes) => {
+        return givenNotes.join('');
+    }
+
+    const addAccidental = (newNote, givenAccidental) => {
+        let copiedNotes = [...newNote];
+        // console.log(givenAccidental);
+        if(newNote.length === 2){
+            return copiedNotes.splice(1, 0, givenAccidental);
+        } else if(newNote.length === 3){
+            return copiedNotes[1] = accidental[0];
+        }
+    }
+
     const handleNoteChange = (note, i, e) => {
-        // e.persist();
-        // console.log(i);
-        // console.log(e.target.value);
-        let newNote = note.split("");
-        newNote[0] = e.target.value;
-        // console.log(newNote);
-
-        let joinedNote = newNote.join('');
-        // console.log(joinedNote);
-        
         let copyNoteCollection = [...musicNotes];
-        copyNoteCollection[i] = joinedNote;
+        
+        if(typeof(note) === 'object'){
+            console.log(note);
+            copyNoteCollection[i] = note;
+            setMusicNotes(copyNoteCollection);
 
-        setMusicNotes(copyNoteCollection);
+        } else {
+            let newNote = note.split("");
+            newNote[0] = e.target.value;
+            
+            copyNoteCollection[i] = joinNotes(newNote);
+            setMusicNotes(copyNoteCollection);
+        }
+        
+    }
+
+    const handleAccidentalChange = (note, i, e) => {
+        let copyNoteCollection = [...musicNotes];
+        let updatedNote;
+
+        if(typeof(note) === 'object'){
+            console.log('resting');
+        } else{
+            let newNote = note.split("");
+            switch(e.target.value){
+                case "Natural":
+                    updatedNote = newNote.filter(item => {
+                        return (item !== "b") && (item !== "#")
+                    });
+                    break;
+                case "#":
+                    updatedNote = addAccidental(newNote, accidental[0])
+                    break;
+                case "b":
+                    // updatedNote = [...newNote];
+                    // if(newNote.length === 2){
+                    //     updatedNote.splice(1, 0, accidental[0]);
+                    // } else if(newNote.length === 3){
+                    //     updatedNote[1] = accidental[0];
+                    // }
+                    updatedNote = addAccidental(newNote, accidental[1])
+                    console.log("Add b");
+                    break;
+                default:
+                    break;
+            }
+            console.log(updatedNote);
+            // copyNoteCollection[i] = joinNotes(updatedNote);
+            // setMusicNotes(copyNoteCollection);
+        }
+
+        
 
     }
 
     const showComposition = () => {
-        return musicNotes.map((note, i) => {
-            
+        return musicNotes.map((note, i) => {   
             return (
                 <div className="note-container">
                     <label>{typeof(note) === 'object' ? "Rest" : note}</label>
-                    <div>
+                    <div onChange={(e) => handleAccidentalChange(note, i, e)}>
                         <input type="radio" value="Natural" name="accidentalHolder"/>Natural
                         <input type="radio" value={accidental[0]} name="accidentalHolder"/>{accidental[0]}
                         <input type="radio" value={accidental[1]} name="accidentalHolder"/>{accidental[1]}
@@ -100,9 +131,7 @@ const Composition = () => {
                     <select onChange={(e) => handleNoteChange(note, i, e)}>
                         {showAvailableNotes(note)}
                     </select>
-                </div>
-                
-                
+                </div>           
             )
         })
     }
